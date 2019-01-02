2d4df8a5aa4dae2859bb0d07fc7d0573cf589846
mpeng <wensheng.peng@gmail.com>
2019-01-02 08:07:30 -0500

    Update images

--- index.android.js
+++ index.android.js
@@ -9,18 +9,35 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
+  Image,
   View
 } from 'react-native';
+
+import { App } from './app';
 
 export default class RNRedux extends Component {
   render() {
     return (
+      
       <View style={styles.container}>
+           <Image
+              source={
+                __DEV__
+                  ? require('./assets/images/robot-dev.png')
+                  : require('./assets/images/robot-prod.png')
+              }
+              style={styles.welcomeImage}
+            />
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+        />
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-		  Welcome to Michael's second Android Native App !
+		  Welcome to Michael's third 5 Android Native App !
           To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
@@ -28,6 +45,7 @@ export default class RNRedux extends Component {
           Shake or press menu button for dev menu
         </Text>
       </View>
+      
     );
   }
 }
@@ -44,11 +62,26 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+   welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  input: {
+    backgroundColor: '#ffffff',
+    height: 55,
+    width: 100,
+    borderRadius: 3,
+    padding: 5,
+    marginTop: 12,
+  }
 });
 
 AppRegistry.registerComponent('RNRedux', () => RNRedux);
